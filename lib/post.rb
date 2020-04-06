0609963e913084bc7c0803e4e15e2e8674b624ea
Nicholas Keller <nic.keller328@gmail.com>
2020-04-06 19:24:47 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+class Post
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author
+    @author
+  end
+  
+  def author_name
+      if self.author.nil?
+        nil
+      else
+        self.author.name
+      end
+  end
+  
+  def save
+    @@all << self
+  end
+end
