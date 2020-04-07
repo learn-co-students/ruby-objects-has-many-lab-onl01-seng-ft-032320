c7eacb041ae848c6876343a1d61b7a18f1dbcf48
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-07 16:33:02 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,32 @@
-class Artist 
+require "pry"
+class Artist #artist should have many songs 
   attr_accessor :name 
   
   def initialize(name)
-    @name = name 
+    @name = name
   end 
   
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def songs # return the songs that belong to the instant of this artist
+    Song.all.select { |song| song.artist == self} # retuns all the values on the @@all class variable of song and filter base on intance of artist class 
   end 
+  #In order to have an artist find all of it's songs, the song class needs to know about all its song instances and a song instance needs to know about the artist class it belongs to.
   
   def add_song(song)
-    song.artist = self
+    song.artist = self 
   end 
   
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self 
+  
+  
+  def add_song_by_name(name)     #need to provide argument since it will create a new song and the  initialize method requires this argument to create the song.
+    song = Song.new(name)    #this is now an object 
+    song.artist = self     #setting the instance of the song class setter method artist equals to the instance of the object. This associates both of the objects.
+    
   end 
   
   def self.song_count 
     Song.all.length
+    
   end 
-  
 end 
 
 
--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,25 @@
+require "pry"
 
-class Song
-  attr_accessor :artist, :name
+class Song # a song must have an artist 
+ attr_accessor :artist, :name 
  
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self 
-  end
-
-  def self.all
-    @@all
-  end
-  
-  def artist_name
-    if self.artist
-      self.artist.name
-    else 
-      nil 
-    end 
+ @@all = []
+ 
+ def initialize(name)
+   @name = name 
+   @@all << self 
+ end 
+ 
+ def self.all 
+   @@all 
+ end 
+ 
+ def artist_name
+  if self.artist # if the instance of an artist exist return true and enter the if statement
+   self.artist.name
+  else 
+    nil
   end 
-  
-end
+ end 
+end 
+
